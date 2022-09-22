7865120a530ac46c9d574c33c65ee8f799558f31
Joe <jothivaananwebdeveloper@gmail.com>
2022-09-22 22:32:15 +0530

    Delete Bug Fix Sep-22

--- inventory/src/main/java/com/inventory/Controller/APIController.java
+++ inventory/src/main/java/com/inventory/Controller/APIController.java
@@ -1,9 +1,14 @@
 package com.inventory.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,28 +37,26 @@ public class APIController {
         return repository.findAll();
     }
 
-//    @CrossOrigin(origins = "http://localhost:8080")
-//     @GetMapping("id")
-//     private Optional<InventoryDetails> getById (@RequestParam ("id") int id) {
-//         return repository.findById(id);
-//     }
+    @GetMapping("id")
+    private Optional<InventoryDetails> getById (@RequestParam ("id") int id) {
+        return repository.findById(id);
+    }
  
-//    @CrossOrigin(origins = "http://localhost:8080")
-//     @PostMapping("data")
-//     private InventoryDetails saveById (@RequestBody InventoryDetails details) {
-//         return repository.save(details);
-//     }
 
-//    @CrossOrigin(origins = "http://localhost:8080")
-//     @PutMapping("data")
-//     private InventoryDetails updateById (@RequestBody InventoryDetails details) {
-//         return repository.save(details);
-//     }
+    @PostMapping("data")
+    private InventoryDetails saveById (@RequestBody InventoryDetails details) {
+        return repository.save(details);
+    }
 
-//    @CrossOrigin(origins = "http://localhost:8080")
-//     @DeleteMapping("data")
-//     private void removeById (@RequestParam ("id") int id) {
-//          repository.deleteById(id);
-//     }
+    @PutMapping("data")
+    private InventoryDetails updateById (@RequestBody InventoryDetails details) {
+        return repository.save(details);
+    }
+
+   
+    @DeleteMapping("data")
+    private void removeById (@RequestParam ("id") int id) {
+         repository.deleteById(id);
+    }
 
 }
--- inventory/src/main/resources/static/dist/js/index.js
+++ inventory/src/main/resources/static/dist/js/index.js
@@ -1,11 +1,81 @@
 $(document).ready (function () {
+
+    let dataList;
+    let deleteId;
+
     inventoryList();
+
+    // $(function () {
+    //     alert(dataList);
+
+       
+        // $('#example2').DataTable({
+        //   "paging": true,
+        //   "lengthChange": false,
+        //   "searcgong": false,
+        //   "ordering": true,
+        //   "info": true,
+        //   "autoWidth": false,
+        //   "responsive": true,
+        // });
+    //   });
 });
+
+const addDetails = () => {
+
+    let inventoryDetails = Array.from(document.querySelectorAll('#form-id input')).reduce((acc, input) => ({...acc,[input.id]:input.value}),{});
+
+    $.ajax({
+
+        url: http+"/data",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(inventoryDetails),
+
+        success: function(successData) {
+
+            resetForm();
+            inventoryList();
+            toastr.info('Details added successfully.', 'Done!', {timeOut: 3000});
+        }
+    })
+}
+
+const generateTable = (dataList) => {
+
+    $("#example1").DataTable({
+        "destroy": true,
+        "responsive": true,
+        "lengthChange": false,
+         "autoWidth": false,
+                 
+          "data": dataList,
+
+          "columns": [
+           {"data": "employeeCode"},
+           {"data": "employeeName"},
+           {"data": "systemInfo"},
+           {"data": "headphoneInfo"},
+           {"data": "keyboardInfo"},
+           {"data": "mouseInfo"},
+           {"data": "laptopStand"},
+           {
+            "data":"id",
+                render: function(dataId) {
+                    return '<button class="btn btn-secondary btn-sm" onClick="deleteConfirmation('+dataId+')">Delete</button>';
+                }
+            }
+          ],
+
+       buttons: ["excel","colvis"]
+      
+
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+}
 
 
 const inventoryList = () => {
-    alert("INSIDE DATA")
-
+   
     $.ajax({
         type: "GET",
         url: http+"/data",  
@@ -13,6 +83,35 @@ const inventoryList = () => {
         
         success: function(successData) {
             console.log("LIST",successData);
+            dataList = successData;
+            console.log("DATALIST",dataList);
+            generateTable(dataList);
+        },
+
+        error: function(error) {
+            console.log("ERROR", error);
+        }
+    });
+}
+
+const deleteConfirmation = (id) => {
+    deleteId = id;
+    $('#delete-model').modal('show');
+}
+
+const deleteDetails = () => {
+    alert(deleteId);
+    $('#delete-model').modal('hide');
+
+    $.ajax({
+        type: "DELETE",
+        url: http+"/data?id="+deleteId,  
+        contentType: "application/json; charset=utf-8",
+        
+        success: function(successData) {
+            alert(successData);
+            toastr.info('Record deleted successfully.', 'Done!', {timeOut: 3000});
+            inventoryList();
         },
 
         error: function(error) {
@@ -22,4 +121,10 @@ const inventoryList = () => {
 }
 
 
+const resetForm = () => {
+
+    var validator = $( "#form-id" ).validate();
+    validator.resetForm();  
+    $('#form-card').CardWidget('toggle');
+}
 
